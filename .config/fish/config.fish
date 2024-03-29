0f6a88745884df9a2ad2673b82d8ee1c8f5e8249
Bayron Monsalvez <bayronmonsalvez@gmail.com>
2024-03-29 03:47:56 -0300

    add stow

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+# Suppresses fish's intro message
+set fish_greeting
+#init starship prompt 
+starship init fish | source
+
+alias ls="exa -l --icons --group-directories-first"
+alias la="exa -la --icons --group-directories-first"
+alias emacs="emacsclient -c -a 'emacs' "
